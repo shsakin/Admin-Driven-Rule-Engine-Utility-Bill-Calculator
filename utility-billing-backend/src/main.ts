7c1361712ca8b4f6c14b63a1a6225be83264aff1
Samsul Haque Sakin <shsakin2002@gmail.com>
2025-12-24 11:30:32 +0600

    final c

--- utility-billing-backend/src/main.ts
+++ utility-billing-backend/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
       'https://admin-driven-bill-calculator-git-main-sakins-projects-4a24a43c.vercel.app',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Auth-Token'],
   });
 
   await app.listen(process.env.PORT || 3000);
